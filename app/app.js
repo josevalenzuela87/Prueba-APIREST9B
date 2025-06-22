f66668353e55400d8684f3c261ccbc0a0dcbd077
Jose Valenzuela <armandovo1993@gmail.com>
2025-06-21 19:50:19 -0600

    Primer commit

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/gymRouter');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
+
+app.use('/gym', router);
+  
+
+module.exports = app;
